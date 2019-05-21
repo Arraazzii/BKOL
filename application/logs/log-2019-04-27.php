36e26a23194ed28778865d817b64249ff657d144
Arraazzii <mumaraziz2014@gmail.com>
2019-05-21 16:00:50 +0700

    Pengembangan

---
+++ application/logs/log-2019-04-27.php
@@ -0,0 +1,10 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2019-04-27 08:48:49 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead C:\xampp\htdocs\BKOL\system\database\drivers\mysql\mysql_driver.php 91
+ERROR - 2019-04-27 08:49:03 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead C:\xampp\htdocs\BKOL\system\database\drivers\mysql\mysql_driver.php 91
+ERROR - 2019-04-27 08:49:03 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead C:\xampp\htdocs\BKOL\system\database\drivers\mysql\mysql_driver.php 91
+ERROR - 2019-04-27 08:49:03 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead C:\xampp\htdocs\BKOL\system\database\drivers\mysql\mysql_driver.php 91
+ERROR - 2019-04-27 08:49:04 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead C:\xampp\htdocs\BKOL\system\database\drivers\mysql\mysql_driver.php 91
+ERROR - 2019-04-27 08:49:32 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead C:\xampp\htdocs\BKOL\system\database\drivers\mysql\mysql_driver.php 91
+ERROR - 2019-04-27 08:49:33 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead C:\xampp\htdocs\BKOL\system\database\drivers\mysql\mysql_driver.php 91
+ERROR - 2019-04-27 08:49:35 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead C:\xampp\htdocs\BKOL\system\database\drivers\mysql\mysql_driver.php 91
