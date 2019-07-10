b04bbe96ee293d103ca68d0b21bbbac8eee8c52b
langpp <gilangpermana1407@gmail.com>
2019-07-10 21:23:18 +0700

    ended

---
+++ application/logs/log-2019-07-05.php
@@ -0,0 +1,49 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2019-07-05 21:49:57 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead C:\xampp\htdocs\bkol\system\database\drivers\mysql\mysql_driver.php 91
+ERROR - 2019-07-05 21:49:59 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead C:\xampp\htdocs\bkol\system\database\drivers\mysql\mysql_driver.php 91
+ERROR - 2019-07-05 21:50:22 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead C:\xampp\htdocs\bkol\system\database\drivers\mysql\mysql_driver.php 91
+ERROR - 2019-07-05 21:50:23 --> Severity: Notice  --> Undefined variable: input C:\xampp\htdocs\bkol\application\views\pencaker\register.php 188
+ERROR - 2019-07-05 21:50:23 --> Severity: Notice  --> Undefined variable: input C:\xampp\htdocs\bkol\application\views\pencaker\register.php 188
+ERROR - 2019-07-05 21:50:23 --> Severity: Notice  --> Undefined variable: input C:\xampp\htdocs\bkol\application\views\pencaker\register.php 188
+ERROR - 2019-07-05 21:50:23 --> Severity: Notice  --> Undefined variable: input C:\xampp\htdocs\bkol\application\views\pencaker\register.php 188
+ERROR - 2019-07-05 21:50:23 --> Severity: Notice  --> Undefined variable: input C:\xampp\htdocs\bkol\application\views\pencaker\register.php 188
+ERROR - 2019-07-05 21:50:23 --> Severity: Notice  --> Undefined variable: input C:\xampp\htdocs\bkol\application\views\pencaker\register.php 210
+ERROR - 2019-07-05 21:50:23 --> Severity: Notice  --> Undefined variable: input C:\xampp\htdocs\bkol\application\views\pencaker\register.php 210
+ERROR - 2019-07-05 21:50:23 --> Severity: Notice  --> Undefined variable: input C:\xampp\htdocs\bkol\application\views\pencaker\register.php 210
+ERROR - 2019-07-05 21:50:23 --> Severity: Notice  --> Undefined variable: input C:\xampp\htdocs\bkol\application\views\pencaker\register.php 210
+ERROR - 2019-07-05 21:50:23 --> Severity: Notice  --> Undefined variable: input C:\xampp\htdocs\bkol\application\views\pencaker\register.php 247
+ERROR - 2019-07-05 21:50:23 --> Severity: Notice  --> Undefined variable: input C:\xampp\htdocs\bkol\application\views\pencaker\register.php 247
+ERROR - 2019-07-05 21:50:23 --> Severity: Notice  --> Undefined variable: input C:\xampp\htdocs\bkol\application\views\pencaker\register.php 247
+ERROR - 2019-07-05 21:50:23 --> Severity: Notice  --> Undefined variable: input C:\xampp\htdocs\bkol\application\views\pencaker\register.php 247
+ERROR - 2019-07-05 21:50:23 --> Severity: Notice  --> Undefined variable: input C:\xampp\htdocs\bkol\application\views\pencaker\register.php 247
+ERROR - 2019-07-05 21:50:23 --> Severity: Notice  --> Undefined variable: input C:\xampp\htdocs\bkol\application\views\pencaker\register.php 247
+ERROR - 2019-07-05 21:50:23 --> Severity: Notice  --> Undefined variable: input C:\xampp\htdocs\bkol\application\views\pencaker\register.php 247
+ERROR - 2019-07-05 21:50:23 --> Severity: Notice  --> Undefined variable: input C:\xampp\htdocs\bkol\application\views\pencaker\register.php 247
+ERROR - 2019-07-05 21:50:23 --> Severity: Notice  --> Undefined variable: input C:\xampp\htdocs\bkol\application\views\pencaker\register.php 247
+ERROR - 2019-07-05 21:50:23 --> Severity: Notice  --> Undefined variable: input C:\xampp\htdocs\bkol\application\views\pencaker\register.php 247
+ERROR - 2019-07-05 21:50:23 --> Severity: Notice  --> Undefined variable: input C:\xampp\htdocs\bkol\application\views\pencaker\register.php 247
+ERROR - 2019-07-05 21:50:23 --> Severity: Notice  --> Undefined variable: input C:\xampp\htdocs\bkol\application\views\pencaker\register.php 361
+ERROR - 2019-07-05 21:50:23 --> Severity: Notice  --> Undefined variable: input C:\xampp\htdocs\bkol\application\views\pencaker\register.php 361
+ERROR - 2019-07-05 21:50:23 --> Severity: Notice  --> Undefined variable: input C:\xampp\htdocs\bkol\application\views\pencaker\register.php 361
+ERROR - 2019-07-05 21:50:23 --> Severity: Notice  --> Undefined variable: input C:\xampp\htdocs\bkol\application\views\pencaker\register.php 361
+ERROR - 2019-07-05 21:50:23 --> Severity: Notice  --> Undefined variable: input C:\xampp\htdocs\bkol\application\views\pencaker\register.php 361
+ERROR - 2019-07-05 21:50:23 --> Severity: Notice  --> Undefined variable: input C:\xampp\htdocs\bkol\application\views\pencaker\register.php 361
+ERROR - 2019-07-05 21:50:23 --> Severity: Notice  --> Undefined variable: input C:\xampp\htdocs\bkol\application\views\pencaker\register.php 361
+ERROR - 2019-07-05 21:50:23 --> Severity: Notice  --> Undefined variable: input C:\xampp\htdocs\bkol\application\views\pencaker\register.php 361
+ERROR - 2019-07-05 21:50:23 --> Severity: Notice  --> Undefined variable: input C:\xampp\htdocs\bkol\application\views\pencaker\register.php 361
+ERROR - 2019-07-05 21:50:23 --> Severity: Notice  --> Undefined variable: input C:\xampp\htdocs\bkol\application\views\pencaker\register.php 361
+ERROR - 2019-07-05 21:50:23 --> Severity: Notice  --> Undefined variable: input C:\xampp\htdocs\bkol\application\views\pencaker\register.php 361
+ERROR - 2019-07-05 21:50:23 --> Severity: Notice  --> Undefined variable: input C:\xampp\htdocs\bkol\application\views\pencaker\register.php 361
+ERROR - 2019-07-05 21:50:23 --> Severity: Notice  --> Undefined variable: input C:\xampp\htdocs\bkol\application\views\pencaker\register.php 361
+ERROR - 2019-07-05 21:50:23 --> Severity: Notice  --> Undefined variable: input C:\xampp\htdocs\bkol\application\views\pencaker\register.php 361
+ERROR - 2019-07-05 21:50:23 --> Severity: Notice  --> Undefined variable: input C:\xampp\htdocs\bkol\application\views\pencaker\register.php 361
+ERROR - 2019-07-05 21:50:23 --> Severity: Notice  --> Undefined variable: input C:\xampp\htdocs\bkol\application\views\pencaker\register.php 361
+ERROR - 2019-07-05 21:50:23 --> Severity: Notice  --> Undefined variable: input C:\xampp\htdocs\bkol\application\views\pencaker\register.php 361
+ERROR - 2019-07-05 21:50:23 --> Severity: Notice  --> Undefined variable: input C:\xampp\htdocs\bkol\application\views\pencaker\register.php 361
+ERROR - 2019-07-05 21:50:23 --> Severity: Notice  --> Undefined variable: input C:\xampp\htdocs\bkol\application\views\pencaker\register.php 361
+ERROR - 2019-07-05 21:50:23 --> Severity: Notice  --> Undefined variable: input C:\xampp\htdocs\bkol\application\views\pencaker\register.php 361
+ERROR - 2019-07-05 21:50:46 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead C:\xampp\htdocs\bkol\system\database\drivers\mysql\mysql_driver.php 91
+ERROR - 2019-07-05 21:51:03 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead C:\xampp\htdocs\bkol\system\database\drivers\mysql\mysql_driver.php 91
+ERROR - 2019-07-05 21:51:03 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead C:\xampp\htdocs\bkol\system\database\drivers\mysql\mysql_driver.php 91
+ERROR - 2019-07-05 21:51:04 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead C:\xampp\htdocs\bkol\system\database\drivers\mysql\mysql_driver.php 91
---
+++ application/logs/log-2019-07-08.php
@@ -0,0 +1,9 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2019-07-08 11:34:57 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead C:\xampp\htdocs\bkol\system\database\drivers\mysql\mysql_driver.php 91
+ERROR - 2019-07-08 11:34:57 --> The session cookie data did not match what was expected. This could be a possible hacking attempt.
+ERROR - 2019-07-08 11:35:02 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead C:\xampp\htdocs\bkol\system\database\drivers\mysql\mysql_driver.php 91
+ERROR - 2019-07-08 11:35:03 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead C:\xampp\htdocs\bkol\system\database\drivers\mysql\mysql_driver.php 91
+ERROR - 2019-07-08 11:35:04 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead C:\xampp\htdocs\bkol\system\database\drivers\mysql\mysql_driver.php 91
+ERROR - 2019-07-08 11:35:08 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead C:\xampp\htdocs\bkol\system\database\drivers\mysql\mysql_driver.php 91
+ERROR - 2019-07-08 11:35:33 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead C:\xampp\htdocs\bkol\system\database\drivers\mysql\mysql_driver.php 91
